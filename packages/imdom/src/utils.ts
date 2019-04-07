bb6fe1d0b4c9c4064c3190f2f14693fa0146ebe2
Kayo Phoenix <kayo@illumium.org>
2019-04-07 22:48:13 +0500

    Added keyboard events handling helpers

--- packages/imdom/src/utils.ts
+++ packages/imdom/src/utils.ts
@@ -226,3 +226,179 @@ export function document_of(node: Node): Document {
     return node.ownerDocument!;
 }
 
+/** Widely used key codes */
+export const enum KeyCode {
+    Backspace = 8,
+    Tab = 9,
+    Enter = 13,
+    Shift = 16,
+    Ctrl = 17,
+    Alt = 18,
+    PauseBreak = 19,
+    CapsLock = 20,
+    Escape = 27,
+    Space = 32,
+    PageUp = 33,
+    PageDown = 34,
+    End = 35,
+    Home = 36,
+
+    LeftArrow = 37,
+    UpArrow = 38,
+    RightArrow = 39,
+    DownArrow = 40,
+
+    Insert = 45,
+    Delete = 46,
+
+    Zero = 48,
+    ClosedParen = Zero,
+    One = 49,
+    ExclamationMark = One,
+    Two = 50,
+    AtSign = Two,
+    Three = 51,
+    PoundSign = Three,
+    Hash = PoundSign,
+    Four = 52,
+    DollarSign = Four,
+    Five = 53,
+    PercentSign = Five,
+    Six = 54,
+    Caret = Six,
+    Hat = Caret,
+    Seven = 55,
+    Ampersand = Seven,
+    Eight = 56,
+    Star = Eight,
+    Asterik = Star,
+    Nine = 57,
+    OpenParen = Nine,
+
+    A = 65,
+    B = 66,
+    C = 67,
+    D = 68,
+    E = 69,
+    F = 70,
+    G = 71,
+    H = 72,
+    I = 73,
+    J = 74,
+    K = 75,
+    L = 76,
+    M = 77,
+    N = 78,
+    O = 79,
+    P = 80,
+    Q = 81,
+    R = 82,
+    S = 83,
+    T = 84,
+    U = 85,
+    V = 86,
+    W = 87,
+    X = 88,
+    Y = 89,
+    Z = 90,
+
+    LeftWindowKey = 91,
+    RightWindowKey = 92,
+    SelectKey = 93,
+
+    Numpad0 = 96,
+    Numpad1 = 97,
+    Numpad2 = 98,
+    Numpad3 = 99,
+    Numpad4 = 100,
+    Numpad5 = 101,
+    Numpad6 = 102,
+    Numpad7 = 103,
+    Numpad8 = 104,
+    Numpad9 = 105,
+
+    Multiply = 106,
+    Add = 107,
+    Subtract = 109,
+    DecimalPoint = 110,
+    Divide = 111,
+
+    F1 = 112,
+    F2 = 113,
+    F3 = 114,
+    F4 = 115,
+    F5 = 116,
+    F6 = 117,
+    F7 = 118,
+    F8 = 119,
+    F9 = 120,
+    F10 = 121,
+    F11 = 122,
+    F12 = 123,
+
+    NumLock = 144,
+    ScrollLock = 145,
+
+    SemiColon = 186,
+    Equals = 187,
+    Comma = 188,
+    Dash = 189,
+    Period = 190,
+    UnderScore = Dash,
+    PlusSign = Equals,
+    ForwardSlash = 191,
+    Tilde = 192,
+    GraveAccent = Tilde,
+
+    OpenBracket = 219,
+    ClosedBracket = 221,
+    Quote = 222
+}
+
+/** Get input key code */
+export function key_code(event: KeyboardEvent): KeyCode {
+    return event.keyCode;
+}
+
+/** Get input character */
+export function key_char(event: KeyboardEvent): string | undefined {
+    /*
+    const code = event.which == null ? event.keyCode : // IE
+        event.which != 0 && event.charCode != 0 ? event.which : // !IE
+            0;
+    */
+    const code = event.charCode != null ? event.charCode : event.keyCode;
+
+    if (code) {
+        if (code < 32) return; // special character
+        return String.fromCharCode(code); // ordinary character
+    }
+
+    // other special characters
+}
+
+/** Common modifier keys */
+export const enum ModKey {
+    Ctrl = 1 << 0,
+    Alt = 1 << 1,
+    Meta = 1 << 2,
+    Shift = 1 << 3,
+}
+
+/** Get active modifier keys */
+export function key_mods(event: KeyboardEvent): ModKey {
+    return (event.ctrlKey ? ModKey.Ctrl : 0) |
+        (event.altKey ? ModKey.Alt : 0) |
+        (event.metaKey ? ModKey.Meta : 0) |
+        (event.shiftKey ? ModKey.Shift : 0);
+}
+
+/** Check for active modifier keys */
+export function mod_keys(event: KeyboardEvent, mask: ModKey): boolean {
+    return ((mask & ModKey.Ctrl) && event.ctrlKey) ||
+        ((mask & ModKey.Alt) && event.altKey) ||
+        ((mask & ModKey.Meta) && event.metaKey) ||
+        ((mask & ModKey.Shift) && event.shiftKey) ||
+        false;
+}
+
